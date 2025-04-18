2de458de806701340e9264959e0f7b74aa00427d
sharmilagreen <sagreen@smith.edu>
2025-04-18 12:46:12 -0400

    Outlined LightForest

---
+++ LightForest.java
@@ -0,0 +1,32 @@
+public class LightForest extends Island implements LightForestRequirements {
+    private boolean shelterBuilt;
+
+    public LightForest() {
+        super("Light Forest", "You are in the Light Forest.");
+        this.shelterBuilt = false;
+    }
+
+    public void forage(){
+
+    }
+    
+    public void collectStick(){
+
+    }
+
+    public void collectRock(){
+
+    }
+
+    public void petAnimal(){
+
+    }
+
+    public void lookAround(){
+
+    }
+
+    public void buildShelter(){
+
+    }
+}
---
+++ LightForestRequirements.java
@@ -0,0 +1,7 @@
+public interface LightForestRequirements {
+    void forage();
+    void collectStick();
+    void collectRock();
+    void petAnimal();
+    void lookAround();
+}
