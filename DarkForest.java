11129af0b6c419f53e3fb13be14fc535d1e41932
Anna <afeuer@smith.edu>
2025-04-27 16:09:46 -0400

    describe()

--- DarkForest.java
+++ DarkForest.java
@@ -58,7 +58,6 @@ public class DarkForest extends Island implements DarkForestRequirements{
             - go north / south / east / west
             - collect rock / stick
             - monkey army
-            - volcanic eruption
             - build fire
             - look around
             - fight
@@ -76,13 +75,12 @@ public class DarkForest extends Island implements DarkForestRequirements{
     }
 
     public void volcanicEruption(){
-
-    }
-
-    public void describe(){
         
     }
 
+    public void describe(){
+        System.out.println(description);
+    }
 
 
     @Override
